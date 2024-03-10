e3650240e136460ea10520cfb23d3ab9c2187de3
AbderrahimZineddine <zenoabderrahim@gmail.com>
2024-03-10 08:43:52 +0100

    edit handling errors

---
+++ src/controller/userController/editMe.ts
@@ -0,0 +1,104 @@
+import catchAsync from '../../utils/catchAsync';
+import AppError from '../../utils/appError';
+import { Response, NextFunction } from 'express';
+import { unlink } from 'node:fs';
+import { promisify } from 'util';
+import { MyRequest } from '../userController';
+import { Role } from '../../models/UserDoc';
+import uploadController from '../../controller/uploadController';
+// import { User } from 'models/User';
+// import { Worker } from 'models/Worker';
+// import { Model } from 'mongoose';
+// import { WorkerDoc } from 'models/WorkerDoc';
+
+export const editMe = (role: any) => {
+  return catchAsync(
+    async (req: MyRequest, res: Response, next: NextFunction) => {
+      try {
+        if (
+          req.body.authentication &&
+          (req.body.authentication.password ||
+            req.body.authentication.passwordConfirm)
+        ) {
+          if (req.profilePicture) {
+            await uploadController.deleteFromCloudinary(req.profilePicture);
+          }
+          return next(
+            new AppError(
+              'This route is not for password updates. Please use /updateMyPassword',
+              400
+            )
+          );
+        }
+
+        if (
+          req.user.role === Role.Worker &&
+          (req.body.firstName || req.body.lastName)
+        ) {
+          if (req.profilePicture) {
+            await uploadController.deleteFromCloudinary(req.profilePicture);
+          }
+          return next(
+            new AppError('You cannot change your first name or last name', 400)
+          ); //TODO check 400 ?
+        }
+
+        const oldPfp = req.user.profilePicture;
+        const updatedUser = await role.findByIdAndUpdate(
+          req.user.id,
+          {
+            firstName: req.body.firstName,
+            lastName: req.body.lastName,
+            wilaya: req.body.wilaya,
+            phoneNumber: req.body.phoneNumber,
+            'contacts.instagram': req.body.contacts
+              ? req.body.contacts.instagram
+              : req.user.contacts.instagram,
+            'contacts.facebook': req.body.contacts
+              ? req.body.contacts.facebook
+              : req.user.contacts.facebook,
+            'contacts.whatsapp': req.body.contacts
+              ? req.body.contacts.whatsapp
+              : req.user.contacts.whatsapp,
+            'contacts.linkedin': req.body.contacts
+              ? req.body.contacts.linkedin
+              : req.user.contacts.linkedin,
+            profilePicture: req.profilePicture ?? req.user.profilePicture,
+          },
+          { new: true, runValidators: true }
+        );
+
+        if (updatedUser.profilePicture != oldPfp) {
+          await uploadController.deleteFromCloudinary(oldPfp);
+        }
+
+        res.status(200).json({
+          status: 'success',
+          data: {
+            user: updatedUser,
+          },
+        });
+      } catch (error) {
+        try {
+          
+          if (req.profilePicture) {
+            await uploadController.deleteFromCloudinary(req.profilePicture);
+          }
+        } catch (error) {
+          if (process.env.NODE_ENV !== 'production') {
+            return next(
+              new AppError(
+                `Some uploaded images failed to delete during editing profile: ${error.message}`,
+                500
+              )
+            );
+          } else {
+            return next(new AppError('Error while editing profile', 500));
+          }
+        }
+        
+        return next(new AppError('Error while editing profile', 500));
+      }
+    }
+  );
+};
--- src/controller/userController/editMeUser.ts
+++ src/controller/userController/editMeUser.ts
@@ -1,29 +1,23 @@
-import catchAsync from '../../utils/catchAsync';
 import AppError from '../../utils/appError';
 import { Response, NextFunction } from 'express';
-import { unlink } from 'node:fs';
-import { promisify } from 'util';
+// import { unlink } from 'node:fs';
+// import { promisify } from 'util';
 import { MyRequest } from '../userController';
 import { Role } from '../../models/UserDoc';
 import uploadController from '../../controller/uploadController';
-// import { User } from 'models/User';
-// import { Worker } from 'models/Worker';
-// import { Model } from 'mongoose';
-// import { WorkerDoc } from 'models/WorkerDoc';
 
 export const editMe = (role: any) => {
-  return catchAsync(
-    async (req: MyRequest, res: Response, next: NextFunction) => {
+  return async (req: MyRequest, res: Response, next: NextFunction) => {
+    let oldDeleted = false;
+    try {
       if (
         req.body.authentication &&
         (req.body.authentication.password ||
           req.body.authentication.passwordConfirm)
       ) {
-        return next(
-          new AppError(
-            'This route is not for password updates. Please use /updateMyPassword',
-            400
-          )
+        throw new AppError(
+          'This route is not for password updates. Please use /updateMyPassword',
+          400
         );
       }
 
@@ -31,18 +25,12 @@ export const editMe = (role: any) => {
         req.user.role === Role.Worker &&
         (req.body.firstName || req.body.lastName)
       ) {
-        return next(
-          new AppError('You cannot change your first name or last name', 400)
-        ); //TODO check 400 ?
+        throw new AppError(
+          'You cannot change your first name or last name',
+          400
+        );
       }
 
-      // const oldfilename = req.user.profilePicture;
-      // let model;
-      // if (role === Role.Worker) {
-      //   model = Worker;
-      // } else {
-      //   model = User;
-      // }
       const oldPfp = req.user.profilePicture;
       const updatedUser = await role.findByIdAndUpdate(
         req.user.id,
@@ -54,39 +42,71 @@ export const editMe = (role: any) => {
           'contacts.instagram': req.body.contacts
             ? req.body.contacts.instagram
             : req.user.contacts.instagram,
+
           'contacts.facebook': req.body.contacts
             ? req.body.contacts.facebook
             : req.user.contacts.facebook,
+
           'contacts.whatsapp': req.body.contacts
             ? req.body.contacts.whatsapp
             : req.user.contacts.whatsapp,
+
           'contacts.linkedin': req.body.contacts
             ? req.body.contacts.linkedin
             : req.user.contacts.linkedin,
-          profilePicture: req.profilePicture ?? req.user.profilePicture,
+
+          profilePicture: req.profilePicture ?? oldPfp,
         },
+
         { new: true, runValidators: true }
       );
 
-      if (updatedUser.profilePicture != oldPfp) {
-        await uploadController.deleteFromCloudinary(oldPfp);
+      if (updatedUser && updatedUser.profilePicture != oldPfp) {
+        try {
+          oldDeleted = true;
+
+          await uploadController.deleteFromCloudinary(oldPfp);
+        } catch (error) {
+          if (process.env.NODE_ENV !== 'production') {
+            throw new AppError(
+              'Error while deleting old profile picture from editing user: ' +
+                error.message,
+
+              500
+            );
+          }
+        }
       }
 
-      // try {
-      //   if (req.file && oldfilename != 'default.jpg') {
-      //     // delete the previous image !
-      //     const unlinkPromisified = promisify(unlink);
-      //     await unlinkPromisified(`src/public/img/users/${oldfilename}`);
-      //   }
-      // } catch (error) {
-      //   return next(new AppError('Error deleting old profile picture !', 400)); //400??
-      // }
       res.status(200).json({
         status: 'success',
+
         data: {
           user: updatedUser,
         },
       });
+    } catch (error) {
+      // oldDeleted = false => the user was updated successfully so you shoudn't delete req.pfp (which is now user.pfp)
+      // if error occured while deleting old profile picture it is already handled
+      if (!oldDeleted && req.profilePicture) {
+        try {
+          await uploadController.deleteFromCloudinary(req.profilePicture);
+        } catch (error) {
+          if (process.env.NODE_ENV !== 'production') {
+            throw new AppError(
+              'Error while deleting old profile picture from editing user: ' +
+                error.message,
+              500
+            );
+          }
+        }
+      }
+
+      if (error instanceof AppError) {
+        return next(error);
+      } else {
+        return next(new AppError('Error while editing profile', 500));
+      }
     }
-  );
+  };
 };
