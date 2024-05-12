6492c2118905faf6335cac897c1c6f7f8fb5b67f
AbderrahimZineddine <zenoabderrahim@gmail.com>
2024-05-12 18:45:17 +0100

    update post

--- dist/controller/postController/updatePost.js
+++ dist/controller/postController/updatePost.js
@@ -5,63 +5,95 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.updatePost = void 0;
 const catchAsync_1 = __importDefault(require("../../utils/catchAsync"));
-const appError_1 = __importDefault(require("../../utils/appError"));
-const Post_1 = require("../../models/Post");
-const uploadController_1 = __importDefault(require("../uploadController"));
+const Worker_1 = require("../../models/Worker");
 exports.updatePost = (0, catchAsync_1.default)(async (req, res, next) => {
     // add new images
     // delete removed images
     // edit description
-    let urls = req.post.images;
-    const removed = req.body.removedPicturesUrls
-        ? req.body.removedPicturesUrls instanceof Array
-            ? req.body.removedPicturesUrls.length
-            : 1
-        : 0;
-    if (removed > urls.length) {
-        return next(new appError_1.default('Error, you are trying to remove more images than you have in this post!', 400));
-    }
-    if (req.body.removedPicturesUrls) {
-        urls = urls.filter((url) => !req.body.removedPicturesUrls.includes(url));
-        // for (const url in req.body.removedPicturesUrls) {
-        if (req.body.removedPicturesUrls instanceof Array) {
-            for (let i = 0; i < req.body.removedPicturesUrls.length; i++) {
-                try {
-                    await uploadController_1.default.deleteFromCloudinary(req.body.removedPicturesUrls[i]);
-                }
-                catch (error) {
-                    if (process.env.NODE_ENV !== 'production') {
-                        return next(new appError_1.default('Error deleting pictures from cloudinary : ' + error.message, 500));
-                    }
-                }
-            }
+    // let urls = req.post.images;
+    // const removed = req.body.removedPicturesUrls
+    //   ? req.body.removedPicturesUrls instanceof Array
+    //     ? req.body.removedPicturesUrls.length
+    //     : 1
+    //   : 0;
+    // if (removed > urls.length) {
+    //   return next(
+    //     new AppError(
+    //       'Error, you are trying to remove more images than you have in this post!',
+    //       400
+    //     )
+    //   );
+    // }
+    // if (req.body.removedPicturesUrls) {
+    //   urls = urls.filter((url) => !req.body.removedPicturesUrls.includes(url));
+    //   // for (const url in req.body.removedPicturesUrls) {
+    //   if (req.body.removedPicturesUrls instanceof Array) {
+    //     for (let i = 0; i < req.body.removedPicturesUrls.length; i++) {
+    //       try {
+    //         await uploadController.deleteFromCloudinary(
+    //           req.body.removedPicturesUrls[i]
+    //         );
+    //       } catch (error) {
+    //         if (process.env.NODE_ENV !== 'production') {
+    //           return next(
+    //             new AppError(
+    //               'Error deleting pictures from cloudinary : ' + error.message,
+    //               500
+    //             )
+    //           );
+    //         }
+    //       }
+    //     }
+    //   } else {
+    //     try {
+    //       await uploadController.deleteFromCloudinary(
+    //         req.body.removedPicturesUrls
+    //       );
+    //     } catch (error) {
+    //       if (process.env.NODE_ENV !== 'production') {
+    //         return next(
+    //           new AppError(
+    //             'Error deleting pictures from cloudinary : ' + error.message,
+    //             500
+    //           )
+    //         );
+    //       }
+    //     }
+    //   }
+    // }
+    // if (req.images) {
+    //   urls = urls.concat(req.images);
+    // }
+    // req.post.images = urls;
+    // await req.post.save({ validateBeforeSave: false });
+    // const post = await Post.findByIdAndUpdate(
+    //   req.params.id,
+    //   {
+    //     job: req.body.job,
+    //     description: req.body.description,
+    //     price: req.body.price,
+    //     title: req.body.title,
+    //     selectedWorkers: req.body.selectedWorkers,
+    //   },
+    //   { new: true, runValidators: true }
+    // );
+    // const post = await Post.findById(req.params.id);
+    let newList = [];
+    for (const id of req.body.selectedWorkers) {
+        if (await Worker_1.Worker.exists({ id: id })) {
+            newList.push(id);
         }
-        else {
-            try {
-                await uploadController_1.default.deleteFromCloudinary(req.body.removedPicturesUrls);
-            }
-            catch (error) {
-                if (process.env.NODE_ENV !== 'production') {
-                    return next(new appError_1.default('Error deleting pictures from cloudinary : ' + error.message, 500));
-                }
-            }
-        }
     }
-    if (req.images) {
-        urls = urls.concat(req.images);
+    if (req.post.selectedWorkers) {
+        req.post.selectedWorkers = newList;
     }
-    req.post.images = urls;
+    else {
+        req.post.selectedWorkers.push(newList);
+    }
     await req.post.save({ validateBeforeSave: false });
-    const post = await Post_1.Post.findByIdAndUpdate(req.params.id, {
-        job: req.body.job,
-        description: req.body.description,
-        price: req.body.price,
-        title: req.body.title,
-        selectedWorkers: req.body.selectedWorkers,
-    }, { new: true, runValidators: true });
     res.status(200).json({
         status: 'success',
-        post,
+        post: req.post,
     });
 });
 //# sourceMappingURL=updatePost.js.map
--- src/controller/postController/updatePost.ts
+++ src/controller/postController/updatePost.ts
@@ -4,86 +4,101 @@ import catchAsync from '../../utils/catchAsync';
 import AppError from '../../utils/appError';
 import { Post } from '../../models/Post';
 import uploadController from '../uploadController';
+import { Worker } from '../../models/Worker';
 
 export const updatePost = catchAsync(
   async (req: MyRequest, res: Response, next: NextFunction) => {
     // add new images
     // delete removed images
     // edit description
-    let urls = req.post.images;
-    const removed = req.body.removedPicturesUrls
-      ? req.body.removedPicturesUrls instanceof Array
-        ? req.body.removedPicturesUrls.length
-        : 1
-      : 0;
-    if (removed > urls.length) {
-      return next(
-        new AppError(
-          'Error, you are trying to remove more images than you have in this post!',
-          400
-        )
-      );
-    }
+    // let urls = req.post.images;
+    // const removed = req.body.removedPicturesUrls
+    //   ? req.body.removedPicturesUrls instanceof Array
+    //     ? req.body.removedPicturesUrls.length
+    //     : 1
+    //   : 0;
+    // if (removed > urls.length) {
+    //   return next(
+    //     new AppError(
+    //       'Error, you are trying to remove more images than you have in this post!',
+    //       400
+    //     )
+    //   );
+    // }
 
-    if (req.body.removedPicturesUrls) {
-      urls = urls.filter((url) => !req.body.removedPicturesUrls.includes(url));
-      // for (const url in req.body.removedPicturesUrls) {
-      if (req.body.removedPicturesUrls instanceof Array) {
-        for (let i = 0; i < req.body.removedPicturesUrls.length; i++) {
-          try {
-            await uploadController.deleteFromCloudinary(
-              req.body.removedPicturesUrls[i]
-            );
-          } catch (error) {
-            if (process.env.NODE_ENV !== 'production') {
-              return next(
-                new AppError(
-                  'Error deleting pictures from cloudinary : ' + error.message,
-                  500
-                )
-              );
-            }
-          }
-        }
-      } else {
-        try {
-          await uploadController.deleteFromCloudinary(
-            req.body.removedPicturesUrls
-          );
-        } catch (error) {
-          if (process.env.NODE_ENV !== 'production') {
-            return next(
-              new AppError(
-                'Error deleting pictures from cloudinary : ' + error.message,
-                500
-              )
-            );
-          }
-        }
+    // if (req.body.removedPicturesUrls) {
+    //   urls = urls.filter((url) => !req.body.removedPicturesUrls.includes(url));
+    //   // for (const url in req.body.removedPicturesUrls) {
+    //   if (req.body.removedPicturesUrls instanceof Array) {
+    //     for (let i = 0; i < req.body.removedPicturesUrls.length; i++) {
+    //       try {
+    //         await uploadController.deleteFromCloudinary(
+    //           req.body.removedPicturesUrls[i]
+    //         );
+    //       } catch (error) {
+    //         if (process.env.NODE_ENV !== 'production') {
+    //           return next(
+    //             new AppError(
+    //               'Error deleting pictures from cloudinary : ' + error.message,
+    //               500
+    //             )
+    //           );
+    //         }
+    //       }
+    //     }
+    //   } else {
+    //     try {
+    //       await uploadController.deleteFromCloudinary(
+    //         req.body.removedPicturesUrls
+    //       );
+    //     } catch (error) {
+    //       if (process.env.NODE_ENV !== 'production') {
+    //         return next(
+    //           new AppError(
+    //             'Error deleting pictures from cloudinary : ' + error.message,
+    //             500
+    //           )
+    //         );
+    //       }
+    //     }
+    //   }
+    // }
+    // if (req.images) {
+    //   urls = urls.concat(req.images);
+    // }
+
+    // req.post.images = urls;
+    // await req.post.save({ validateBeforeSave: false });
+
+    // const post = await Post.findByIdAndUpdate(
+    //   req.params.id,
+    //   {
+    //     job: req.body.job,
+    //     description: req.body.description,
+    //     price: req.body.price,
+    //     title: req.body.title,
+    //     selectedWorkers: req.body.selectedWorkers,
+    //   },
+    //   { new: true, runValidators: true }
+    // );
+    // const post = await Post.findById(req.params.id);
+
+    let newList = [];
+    for (const id of req.body.selectedWorkers) {
+      if (await Worker.exists({ id: id })) {
+        newList.push(id);
       }
     }
-    if (req.images) {
-      urls = urls.concat(req.images);
+    if (req.post.selectedWorkers) {
+      req.post.selectedWorkers = newList;
+    } else {
+      req.post.selectedWorkers.push(newList as any);
     }
-
-    req.post.images = urls;
     await req.post.save({ validateBeforeSave: false });
-
-    const post = await Post.findByIdAndUpdate(
-      req.params.id,
-      {
-        job: req.body.job,
-        description: req.body.description,
-        price: req.body.price,
-        title: req.body.title,
-        selectedWorkers: req.body.selectedWorkers,
-      },
-      { new: true, runValidators: true }
-    );
 
     res.status(200).json({
       status: 'success',
-      post,
+      post: req.post,
     });
   }
 );
