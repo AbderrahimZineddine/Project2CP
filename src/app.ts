03ae7f3fc56fc156f4bca72ded4ab3ed237d7e37
AbderrahimZineddine <zenoabderrahim@gmail.com>
2024-05-13 15:13:55 +0100

    cors

--- src/app.ts
+++ src/app.ts
@@ -41,7 +41,7 @@ app.use(bodyParser.json());
 
 const allowedOrigins = [
   'http://localhost:5174',
-  'http://127.0.0.1:5173',
+  'http://localhost:5173',
   'https://easyhome-lcvx.onrender.com',
   '*',
 ];
